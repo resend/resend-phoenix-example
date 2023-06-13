cb6e71c8c5a3a41db56005ed08f71b08f0d59aa0
Justin Tormey <justin@upsidefinancing.com>
2023-06-12 21:20:19 -0500

    Install :resend dep

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule App.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:resend, "~> 0.2"},
       {:phoenix, "~> 1.7.3"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
